0020ffde83a151280bb7c85699bc58abbcc43cfc
Fábio Mello <fabiojuniormello97@gmail.com>
2021-02-01 00:14:20 -0300

    Urna Eletrônica

---
+++ js/etapas_urna.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos:[
+                    {url:'38111.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos:[
+                    {url:'77222.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos:[
+        {
+            numero: '99',
+            nome: 'Cicero',
+            partido: 'ABC',
+            vice: 'Cic',
+            fotos:[
+                {url:'prefeito1.png', legenda: 'Prefeito'},
+                {url: 'vice-prefeito1.png', legenda: 'Vice-Prfeito', small: true}
+            ]
+        },
+        {
+            numero: '84',
+            nome: 'Zulano',
+            partido: 'PSDB',
+            vice: 'Irineu',
+            fotos:[
+                {url:'prefeito2.png', legenda: 'Prefeito'},
+                {url: 'vice-prefeito.png', legenda: 'Vice-Prefeito', small: true}
+            ]
+        }    
+        ]
+    },
+];
